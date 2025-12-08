73c9ea9ec17045773ad6fb42959059379d23fc68
intelava <tarkanatakkan@gmail.com>
2025-12-08 09:15:26 +0000

    minor changes on atari

--- demo_atari.py
+++ demo_atari.py
@@ -52,7 +52,7 @@ def run_pong_demo():
         engine = RemoraEngine(image_size=(84, 84), hidden_dim=2048, num_layers=4, num_actions=2)
         
         # Initialize the Gymnasium environment
-        env = gym.make("PongDeterministic-v4", render_mode='human')
+        env = gym.make("PongDeterministic-v4", render_mode='rgb_array')
         observation, info = env.reset(seed=42)
         
     except ImportError as e:
